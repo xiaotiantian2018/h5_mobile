c99288aef9e7aaff40c4e4c895e83fc4530de21e
‘tiansuwen’ <791759913@qq.com>
2022-05-06 20:52:24 +0800

    readme

--- src/components/copyRight/index.js
+++ src/components/copyRight/index.js
@@ -4,8 +4,8 @@ import './index.scss'
 export default ()=>{
     return (
         <div className="copyright">
-            <div className="name"><i className="icon-zan"></i>有赞</div>
-            <div className="spec">有赞提供技术支持</div>
+            <div className="name"><i className="icon-zan"></i></div>
+            <div className="spec">h5购物网站</div>
         </div>
     )
 }
